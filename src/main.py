3dd5693e14c45e8862b010481b94718538c43576
Andrew Obwocha <zeros-faddish.0t@icloud.com>
2026-02-25 22:27:45 -0700

    refactor: change scheduling to cron job

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
-import schedule
-import time
 from services import call_instacart_api
 
 
 if __name__ == "__main__":
-    schedule.every().friday.at("19:30").do(call_instacart_api)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    call_instacart_api()
